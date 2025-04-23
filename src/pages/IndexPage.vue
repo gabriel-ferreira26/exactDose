d866d2e375a4e2f0fbed26791cde21e1488231c0
Gabriel Augusto <gaferreira2000@hotmail.com>
2025-04-22 22:54:28 -0300

    Fazendo calculos e preenchendo a tabela

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -63,24 +63,105 @@
         </q-card-section>
       </q-card>
 
+      <q-markup-table bordered flat class="text-center">
+        <thead>
+          <tr>
+            <td>ANESTÉSICO</td>
+            <td>{{ anesthetic }}</td>
+            <td>CONCENTRAÇÃO</td>
+            <td>{{ concentration }}</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>KILOS</td>
+            <td>{{weight}}</td>
+            <td>MICROG/TUBETE</td>
+            <td>{{defineMicrogramsPerTube()}}</td>
+          </tr>
+          <tr>
+            <td>DOSE MÁX/KG</td>
+            <td>{{defineMaxDoseKG()}}</td>
+            <td>DOSE MÁX.</td>
+            <td>{{defineMaxDose()}}</td>
+          </tr>
+          <tr>
+            <td>DOSE POR TUBETE</td>
+            <td>{{defineDosePerTube()}}</td>
+            <td>Nº DE TUBETES</td>
+            <td>{{defineQuantityTubes(defineMaxDose(), defineMicrogramsPerTube())}}</td>
+          </tr>
+          <tr>
+            <td>Nº DE TUBETES</td>
+            <td>{{defineQuantityTubesTwo(defineMaxDoseKG(), defineDosePerTube())}}</td>
+            <td colspan="2"></td>
+          </tr>
+          <tr>
+            <td>Nº MÁX. TUBETES</td>
+            <td>{{defineMaxTubes()}}</td>
+            <td colspan="2"></td>
+          </tr>
+          <tr>
+            <td colspan="4" class="bg-primary text-white">
+              LIMITE DE TUBETES INDICADO PELO SAL ANESTÉSICO
+            </td>
+          </tr>
+        </tbody>
+      </q-markup-table>
     </div>
   </q-page>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue'
 
-export default {
-  setup () {
-    return {
-      anesthetic: ref(null),
-      options: [
-        "Lido + Vaso", "Mepivacaina", "Prilocaina", "Articaina", "Bupivacaina"
-      ],
-      asa: ref(2),
-      concentration: ref(1),
-      weight: ref(null)
-    }
-  }
+const anesthetic = ref(null)
+const options = ['Lido + Vaso', 'Mepivacaina', 'Prilocaina', 'Articaina', 'Bupivacaina']
+const asa = ref(2)
+const concentration = ref(1)
+const weight = ref(null)
+
+/* Valor dos anestesicos */
+const defineMicrogramsPerTube = () => {
+  if (concentration.value == 1) return 36
+  else if (concentration.value == 2) return 18
+  else if (concentration.value == 3) return 9
+}
+
+const defineMaxDoseKG = () => {
+  if (concentration.value == 1) return 7
+  else if (concentration.value == 2) return 6.6
+  else if (concentration.value == 3) return 8
+}
+
+const defineMaxDose = () => {
+  if (asa.value <= 2) return 198
+  else if (asa.value > 2) return 40
+}
+
+const defineDosePerTube = () => {
+  let index = options.indexOf(anesthetic.value)
+  if (index == 1) return 36
+  else if (index == 2) return 36
+  else if (index == 3) return 72
+  else if (index == 4) return 72
+  else if (index == 5) return 9
+}
+
+const defineQuantityTubes = (maxDose, microgramsPerTube) => {
+  return maxDose / microgramsPerTube
+}
+
+const defineQuantityTubesTwo = (maxDoseKG, dosePerTube) => {
+  return (weight.value * maxDoseKG) / dosePerTube
+}
+
+const defineMaxTubes = () => {
+  let index = options.indexOf(anesthetic.value)
+  if (index == 1) return '13 Tubetes'
+  else if (index == 2) return '11 Tubetes'
+  else if (index == 3) return '8 Tubetes'
+  else if (index == 4) return '6 Tubetes'
+  else if (index == 5) return '10 Tubetes'
 }
 </script>
